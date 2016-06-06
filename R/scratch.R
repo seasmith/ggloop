a64277618c662bc7440d8a87efa83d6e29d9cdaa
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-06 12:11:05 -0500

    Add namespace ggplot2:: to all functions from ggplot2

--- R/scratch.R
+++ R/scratch.R
@@ -27,29 +27,29 @@ split.agg <- function(data, facet = NULL){
 }
 
 
-### create the function to loop through in mapply()
+# create the function to loop through in mapply()
 dist.plot <- function(.data, x.name, facet.exists = TRUE){
-    ### check to see if a facet exists in the data frame
+    # check to see if a facet exists in the data frame
     if(!facet.exists){
         Facet = NULL
     }
-    ### begin writing the plot
-    ggplot(data = .data, mapping = aes(x = Variable, fill = Facet)) +
+    # begin writing the plot
+    ggplot2::ggplot(data = .data, mapping = aes(x = Variable, fill = Facet)) +
 
-        xlab(x.name) +
+        ggplot2::xlab(x.name) +
 
-        geom_histogram(aes(y = ..density..), bins =  50, alpha = .5) +
-        geom_density(aes(color = Facet), alpha = .1) +
+        ggplot2::geom_histogram(aes(y = ..density..), bins =  50, alpha = .5) +
+        ggplot2::geom_density(aes(color = Facet), alpha = .1) +
 
-        party.colors.1 +
-        party.colors.2 +
+        ggplot2::party.colors.1 +
+        ggplot2::party.colors.2 +
 
-        facet_grid(. ~ Facet) +
-        geom_vline(data = .data,
+        ggplot2::facet_grid(. ~ Facet) +
+        ggplot2::geom_vline(data = .data,
                    aes(xintercept = mean(Variable),linetype = "dashed"),
                    size = 1) +
-        geom_vline(data = .data,
+        ggplot2::geom_vline(data = .data,
                    aes(xintercept = median(Variable), linetype="solid"),
                    size = 1) +
-        scale_linetype_identity(guide="legend", label = c("Mean", "Median"))
+        ggplot2::scale_linetype_identity(guide="legend", label = c("Mean", "Median"))
 }
