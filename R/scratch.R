1750a25beed68e7a1b11239bdccf96298fef7539
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-11 00:58:19 -0500

    Deleted texasprimary functions; pasted select(), select_(), and select_vars() definitions

--- R/scratch.R
+++ R/scratch.R
@@ -1,55 +1,37 @@
 
-# Load Dependencies -------------------------------------------------------
-require(ggplot2)
+# Basic mapply() ----------------------------------------------------------
+
+aes.list <- mapply(FUN = aes,
+                   x   = x.vars,
+                   y   = y.vars,
+                   ... = ...vars)
 
 
-# split.agg(cols, facets) -------------------------------------------------
 
+# select ------------------------------------------------------------------
 
-split.agg <- function(data, facet = NULL){
-
-    if(!is.null(facet) & (facet %in% names(data))){
-        data2 <- data[, !names(data) %in% facet]
-    } else{
-        data2 <- data
-    }
-    split.df <- function(x, facet){
-        shdw.list <- data.frame()        ## to hold data in loop
-        shdw.list <- data[,c(x, facet)]
-        colnames(shdw.list) <- c("Variable", "Facet")
-        return(shdw.list)
-    }
-
-    agg.list <- lapply(names(data2), split.df, facet = facet)
-
-    names(agg.list) <- names(data2)              ## name it
-    return(agg.list)
+function (.data, ...)
+{
+  select_(.data, .dots = lazyeval::lazy_dots(...))
 }
+<environment: namespace:dplyr>
 
 
-# create the function to loop through in mapply()
-dist.plot <- function(.data, x.name, facet.exists = TRUE){
-    # check to see if a facet exists in the data frame
-    if(!facet.exists){
-        Facet = NULL
-    }
-    # begin writing the plot
-    ggplot2::ggplot(data = .data, mapping = aes(x = Variable, fill = Facet)) +
+# select_ -----------------------------------------------------------------
 
-        ggplot2::xlab(x.name) +
-
-        ggplot2::geom_histogram(aes(y = ..density..), bins =  50, alpha = .5) +
-        ggplot2::geom_density(aes(color = Facet), alpha = .1) +
-
-        ggplot2::party.colors.1 +
-        ggplot2::party.colors.2 +
-
-        ggplot2::facet_grid(. ~ Facet) +
-        ggplot2::geom_vline(data = .data,
-                   aes(xintercept = mean(Variable),linetype = "dashed"),
-                   size = 1) +
-        ggplot2::geom_vline(data = .data,
-                   aes(xintercept = median(Variable), linetype="solid"),
-                   size = 1) +
-        ggplot2::scale_linetype_identity(guide="legend", label = c("Mean", "Median"))
+function (.data, ..., .dots)
+{
+  UseMethod("select_")
 }
+<environment: namespace:dplyr>
+
+
+
+# select_vars -------------------------------------------------------------
+
+function (vars, ..., include = character(), exclude = character())
+{
+  args <- lazyeval::lazy_dots(...)
+  select_vars_(vars, args, include = include, exclude = exclude)
+}
+<environment: namespace:dplyr>
