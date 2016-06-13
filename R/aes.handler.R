379f29feffc65135b192da6532660b77708a1909
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-13 03:39:42 -0500

    added and lapply() iterator

--- R/aes.handler.R
+++ R/aes.handler.R
@@ -1,8 +1,13 @@
+lapply(seq_along(1:x), function(x, y){
+  to.delete <- (1:y)[-x]
+}, y = x)
+
 
 # Basic mapply() ----------------------------------------------------------
 
-aes.list <- mapply(FUN = ggplot2::aes,
-                   x   = x.vars,
-                   y   = y.vars,
-                   ... = ...vars,
-                   SIMPLIFY = F)
+aes_inputs <- function(data, x.vars, y.vars, ...)
+  mapply(FUN = ggplot2::aes,
+               x   = x.vars,
+               y   = y.vars,
+               ... = ...vars,
+               SIMPLIFY = F)
