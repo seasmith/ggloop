99af21123a02614925e291bc11ff29027533bd7d
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-13 03:41:57 -0500

    added remap handlers to ggloop(); added lapply() and Map() starters to aes_list()

--- R/data.handler.R
+++ R/data.handler.R
@@ -4,19 +4,29 @@
 
 
 ggloop <- function(data,
-                   mapping = aes_list(),
-                   recycle = TRUE,
+                   mapping = aes_loop(),
+                   remap_xy = TRUE,
+                   remap_dots = TRUE,
                    environment = parent.frame()){
+  if(remap_xy) remap_xy_TRUE(mapping)
+  if(!remap_xy) remap_xy_FALSE(mapping)
+  if(is.na(remap_xy)) remap_xy_NA(mapping)
 
+  if(remap_dots) remap_dots_TRUE(mapping)
+  if(!remap_dots) remap_dots_FALSE(mapping)
 }
 
 
 # aes_list() --------------------------------------------------------------
 
 
-aes_list <- function(x,
-                     y,
-                     ...){
+aes_list <- function(lst){
+  dots.num <- length(lst) - 2
+  dots.length <- length(lst[3])
+  lapply(dots.length, function(x){
+    Map(f = aes, x = lst$x, y = lst$y, lst)
+  })
+
   mapply(FUN = ggplot2::aes,
          x = x,
          y = y,
@@ -57,50 +67,9 @@ aes_inputs <- function(data,
 
 
 
-# recycle_TRUE() ----------------------------------------------------------
+# remap_xy_TRUE() -----------------------------------------------------------
 
-
-recycle_TRUE <- function(lst){
-  logic <- c("x", "y") %in% names(lst)
-  if(all(logic)){
-    lengths <- c(length(lst[["x"]]), length(lst[["y"]]))
-    .max <- which.max(lengths)
-    .min <- which.min(lengths)
-
-    quotient <- length(lst[[.max]]) %/% length(lst[[.min]])
-    remainder <- length(lst[[.max]]) %% length(lst[[.min]])
-      if.zero <- !is.na(remainder/remainder) %>% sum() # 0 if 0/0; 1 if !0/0
-
-    .min.quotient <- rep(lst[[.min]], quotient)
-    .min.remainder <- rep(lst[[.min]][1L:remainder], if.zero) # rep 0 or 1
-
-    lst[[.min]] <- c(.min.quotient, .min.remainder)
-  }
-  return(lst)
-}
-
-
-
-# recycle_FALSE() ---------------------------------------------------------
-
-
-recycle_FALSE <- function(lst){
-  logic <- c("x", "y") %in% names(lst)
-  if(all(logic)){
-    lengths <- c(length(lst[["x"]]), length(lst[["y"]]))
-    .max <- which.max(lengths)
-    .min <- which.min(lengths)
-    .max.length <- length(lst[[.max]])
-
-    lst[[.min]] <- lst[[.min]][1L:.max.length]
-  }
-  return(lst)
-}
-
-
-# recycle_NULL() ----------------------------------------------------------
-
-recycle_NULL <- function(lst){
+remap_xy_TRUE <- function(lst){
   logic <- c("x", "y") %in% names(lst)
   if(all(logic)){
     combo <- expand.grid(x = lst$x, y = lst$y, stringsAsFactors = F)
@@ -119,13 +88,70 @@ recycle_NULL <- function(lst){
 }
 
 
-# recycle_dots() ----------------------------------------------------------
+# remap_xy_FALSE() ----------------------------------------------------------
 
 
-recycle_dots <- function(lst){
-  if((length(lst) - 2) == 0){
-    combo <- expand.grid(lst[-(1:2)], stringsAsFactors = F)
-    lst[3:length(lst)] <- combo(1:length(combo))
+remap_xy_FALSE <- function(lst){
+  logic <- c("x", "y") %in% names(lst)
+  if(all(logic)){
+    lengths <- c(length(lst[["x"]]), length(lst[["y"]]))
+    .max <- which.max(lengths)
+    .min <- which.min(lengths)
+    .max.length <- length(lst[[.max]])
+
+    lst[[.min]] <- lst[[.min]][1L:.max.length]
   }
   return(lst)
 }
+
+
+# remap_xy_NA() ---------------------------------------------------------
+
+
+remap_xy_NA <- function(lst){
+  logic <- c("x", "y") %in% names(lst)
+  if(all(logic)){
+    lengths <- c(length(lst[["x"]]), length(lst[["y"]]))
+    .max <- which.max(lengths)
+    .min <- which.min(lengths)
+
+    quotient <- length(lst[[.max]]) %/% length(lst[[.min]])
+    remainder <- length(lst[[.max]]) %% length(lst[[.min]])
+    if.zero <- !is.na(remainder/remainder) %>% sum() # 0 if 0/0; 1 if !0/0
+
+    .min.quotient <- rep(lst[[.min]], quotient)
+    .min.remainder <- rep(lst[[.min]][1L:remainder], if.zero) # rep 0 or 1
+
+    lst[[.min]] <- c(.min.quotient, .min.remainder)
+  }
+  return(lst)
+}
+
+
+# remap_dots_TRUE() ---------------------------------------------------------
+
+
+remap_dots_TRUE <- function(lst){
+  if((length(lst) - 2) > 0){
+    combo <- expand.grid(lst[-(1:2)], stringsAsFactors = F)
+    lst[3:length(lst)] <- combo[1:length(combo)]
+  }
+  return(lst)
+}
+
+
+# remap_dots_FALSE() --------------------------------------------------------
+
+
+remap_dots_FALSE <- function(lst){
+  if((length(lst) - 2) > 0){
+    dots <- lst[3:length(lst)]
+    no.recycle <- sapply(dots, length) %>%
+      which.max() %>%
+      lapply(dots[-.], function(x, y) x[1L:length(dots[[y]])], y = .)
+    lst[names(no.recycle)] <- no.recycle
+    }
+  return(lst)
+  }
+
+
