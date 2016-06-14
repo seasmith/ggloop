bb073781a28970bf96e6d02e668437e6866f6e5c
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-06-13 19:23:30 -0500

    Fix: is.dot >>> is.dots swap expand.grid() inputs switch dupes/dubs with is... in which() add xy prefixes

--- R/data.handler.R
+++ R/data.handler.R
@@ -80,20 +80,20 @@ aes_inputs <- function(data, x, y, ...){
   # capture dots if exist
   dots <- as.list(substitute(list(...)))[-1L]
   if(length(dots)){
-    dot.eval <- lapply(seq_along(dots), function(i){
+    dots.eval <- lapply(seq_along(dots), function(i){
       arg.eval <- data %>% dplyr::select(eval(dots[[i]])) %>% names()
       }) %>%
     magrittr::set_names(names(dots))
-  is.dot <- TRUE
+  is.dots <- TRUE
   } else{
-    dot.eval <- NULL
-    is.dot   <- FALSE
+    dots.eval <- NULL
+    is.dots   <- FALSE
   }
 
   # list logical existance and values (if any) for all arguments
   mappings <- c(is.x = is.x, x = x.eval,
                 is.y = is.y, y = y.eval,
-                is.dot = is.dot, dot.eval)
+                is.dots = is.dots, dots.eval)
   return(mappings)
 }
 
@@ -102,15 +102,15 @@ aes_inputs <- function(data, x, y, ...){
 
 remap_xy_TRUE <- function(lst){
   if(lst$is.x && lst$is.y){
-    xy <- expand.grid(x = lst$x, y = lst$y, stringsAsFactors = F)
+    xy <- expand.grid(y = lst$y, x = lst$x, stringsAsFactors = F)
 
     is.dupes <- mapply(FUN = c, xy$x, xy$y, SIMPLIFY = F) %>%
       lapply(sort) %>%
       duplicated()
-    dupes <- if(sum(is.dupes)) which(dupes) else NULL
+    dupes <- if(sum(is.dupes)) which(is.dupes) else NULL
 
-    is.dubs <- which(xy$x == xy$y)
-    dubs <- if(sum(is.dubs)) which(dubs) else NULL
+    is.dubs <- xy$x == xy$y
+    dubs <- if(sum(is.dubs)) which(is.dubs) else NULL
 
     deletes <- c(dupes, dubs)
     if(!is.null(deletes)) xy <- xy[-deletes, ]
@@ -122,15 +122,15 @@ remap_xy_TRUE <- function(lst){
 }
 
 
-# remap_xy_FALSE() ----------------------------------------------------------
+# remap_xy_NA() -------------------------------------------------------
 
 
-remap_xy_FALSE <- function(lst){
-  if(is.x && is.y){
-    lengths <- c(length(lst[["x"]]), length(lst[["y"]]))
-    xy.max <- whichxy.max(lengths)
-    xy.min <- whichxy.min(lengths)
-    xy.max.length <- length(lst[[xy.max]])
+remap_xy_NA <- function(lst){
+  if(lst$is.x && lst$is.y){
+    xy.lengths <- c(x = length(lst[["x"]]), y = length(lst[["y"]]))
+      xy.max <- which.max(xy.lengths) %>% names()
+      xy.min <- which.min(xy.lengths) %>% names()
+        xy.max.length <- length(lst[[xy.max]])
 
     lst[[xy.min]] <- lst[[xy.min]][1L:xy.max.length]
   }
@@ -138,23 +138,23 @@ remap_xy_FALSE <- function(lst){
 }
 
 
-# remap_xy_NA() ---------------------------------------------------------
+# remap_xy_FALSE() ------------------------------------------------------
 
 
-remap_xy_NA <- function(lst){
-  if(is.x && is.y){
-    lengths <- c(length(lst[["x"]]), length(lst[["y"]]))
-    .max <- which.max(lengths)
-    .min <- which.min(lengths)
+remap_xy_FALSE <- function(lst){
+  if(lst$is.x && lst$is.y){
+    xy.lengths <- c(x = length(lst[[2]]), y = length(lst[[4]]))
+      xy.max <- which.max(xy.lengths) %>% names()
+      xy.min <- which.min(xy.lengths) %>% names()
 
-    quotient <- length(lst[[.max]]) %/% length(lst[[.min]])
-    remainder <- length(lst[[.max]]) %% length(lst[[.min]])
+    quotient <- length(lst[[xy.max]]) %/% length(lst[[xy.min]])
+    remainder <- length(lst[[xy.max]]) %% length(lst[[xy.min]])
     if.zero <- !is.na(remainder/remainder) %>% sum() # 0 if 0/0; 1 if !0/0
 
-    .min.quotient <- rep(lst[[.min]], quotient)
-    .min.remainder <- rep(lst[[.min]][1L:remainder], if.zero) # rep 0 or 1
+    xy.quotient <- rep(lst[[xy.min]], quotient)
+    xy.remainder <- rep(lst[[xy.min]][1L:remainder], if.zero) # rep 0 or 1
 
-    lst[[.min]] <- c(.min.quotient, .min.remainder)
+    lst[[xy.min]] <- c(xy.quotient, xy.remainder)
   }
   return(lst)
 }
@@ -164,9 +164,9 @@ remap_xy_NA <- function(lst){
 
 
 remap_dots_TRUE <- function(lst){
-  if(is.dots){
-    combo <- expand.grid(lst[-(1:2)], stringsAsFactors = F)
-    lst[3:length(lst)] <- combo[1:length(combo)]
+  if(lst$is.dots){
+    dots <- expand.grid(lst[-(1:4)], stringsAsFactors = F)
+    lst[3:length(lst)] <- dots[1:length(dots)]
   }
   return(lst)
 }
@@ -176,8 +176,8 @@ remap_dots_TRUE <- function(lst){
 
 
 remap_dots_FALSE <- function(lst){
-  if(is.dots){
-    dots <- lst[3:length(lst)]
+  if(lst$is.dots){
+    dots <- lst[5:length(lst)]
     no.recycle <- sapply(dots, length) %>%
       which.max() %>%
       lapply(dots[-.], function(x, y) x[1L:length(dots[[y]])], y = .)
