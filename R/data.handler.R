efb8a2ba4cd0dd928c8d2517f68f85b1f1bbe14f
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-14 01:22:54 -0500

    Move remap functions from data.handler.R to newly created remap.R

--- R/data.handler.R
+++ R/data.handler.R
@@ -6,7 +6,7 @@
 ggloop <- function(data,
                    mapping = aes_loop(),
                    remap_xy = TRUE,
-                   remap_dots = TRUE,
+                   remap_dots = FALSE,
                    environment = parent.frame()){
 
 }
@@ -79,7 +79,7 @@ aes_inputs <- function(data, x, y, ...){
 
   # capture dots if exist
   dots <- as.list(substitute(list(...)))[-1L]
-  if(length(dots)){
+  if(length(dots) > 0){
     dots.eval <- lapply(seq_along(dots), function(i){
       arg.eval <- data %>% dplyr::select(eval(dots[[i]])) %>% names()
       }) %>%
@@ -96,94 +96,4 @@ aes_inputs <- function(data, x, y, ...){
                 is.dots = is.dots, dots.eval)
   return(mappings)
 }
-
-
-# remap_xy_TRUE() -----------------------------------------------------------
-
-remap_xy_TRUE <- function(lst){
-  if(lst$is.x && lst$is.y){
-    xy <- expand.grid(y = lst$y, x = lst$x, stringsAsFactors = F)
-
-    is.dupes <- mapply(FUN = c, xy$x, xy$y, SIMPLIFY = F) %>%
-      lapply(sort) %>%
-      duplicated()
-    dupes <- if(sum(is.dupes)) which(is.dupes) else NULL
-
-    is.dubs <- xy$x == xy$y
-    dubs <- if(sum(is.dubs)) which(is.dubs) else NULL
-
-    deletes <- c(dupes, dubs)
-    if(!is.null(deletes)) xy <- xy[-deletes, ]
-
-    lst$x <- xy$x
-    lst$y <- xy$y
-  }
-  return(lst)
-}
-
-
-# remap_xy_NA() -------------------------------------------------------
-
-
-remap_xy_NA <- function(lst){
-  if(lst$is.x && lst$is.y){
-    xy.lengths <- c(x = length(lst[["x"]]), y = length(lst[["y"]]))
-      xy.max <- which.max(xy.lengths) %>% names()
-      xy.min <- which.min(xy.lengths) %>% names()
-        xy.max.length <- length(lst[[xy.max]])
-
-    lst[[xy.min]] <- lst[[xy.min]][1L:xy.max.length]
-  }
-  return(lst)
-}
-
-
-# remap_xy_FALSE() ------------------------------------------------------
-
-
-remap_xy_FALSE <- function(lst){
-  if(lst$is.x && lst$is.y){
-    xy.lengths <- c(x = length(lst[[2]]), y = length(lst[[4]]))
-      xy.max <- which.max(xy.lengths) %>% names()
-      xy.min <- which.min(xy.lengths) %>% names()
-
-    quotient <- length(lst[[xy.max]]) %/% length(lst[[xy.min]])
-    remainder <- length(lst[[xy.max]]) %% length(lst[[xy.min]])
-    if.zero <- !is.na(remainder/remainder) %>% sum() # 0 if 0/0; 1 if !0/0
-
-    xy.quotient <- rep(lst[[xy.min]], quotient)
-    xy.remainder <- rep(lst[[xy.min]][1L:remainder], if.zero) # rep 0 or 1
-
-    lst[[xy.min]] <- c(xy.quotient, xy.remainder)
-  }
-  return(lst)
-}
-
-
-# remap_dots_TRUE() ---------------------------------------------------------
-
-
-remap_dots_TRUE <- function(lst){
-  if(lst$is.dots){
-    dots <- expand.grid(lst[-(1:4)], stringsAsFactors = F)
-    lst[3:length(lst)] <- dots[1:length(dots)]
-  }
-  return(lst)
-}
-
-
-# remap_dots_FALSE() --------------------------------------------------------
-
-
-remap_dots_FALSE <- function(lst){
-  if(lst$is.dots){
-    dots <- lst[5:length(lst)]
-    no.recycle <- sapply(dots, length) %>%
-      which.max() %>%
-      lapply(dots[-.], function(x, y) x[1L:length(dots[[y]])], y = .)
-    lst[names(no.recycle)] <- no.recycle
-    }
-  return(lst)
-  }
-
 
---
+++ R/remap.R
@@ -0,0 +1,94 @@
+# remap_xy_TRUE() -----------------------------------------------------------
+
+remap_xy_TRUE <- function(lst){
+  if(lst$is.x && lst$is.y){
+    xy <- expand.grid(y = lst$y, x = lst$x, stringsAsFactors = F)
+
+    is.dupes <- mapply(FUN = c, xy$x, xy$y, SIMPLIFY = F) %>%
+      lapply(sort) %>%
+      duplicated()
+    dupes <- if(sum(is.dupes)) which(is.dupes) else NULL
+
+    is.dubs <- xy$x == xy$y
+    dubs <- if(sum(is.dubs)) which(is.dubs) else NULL
+
+    deletes <- c(dupes, dubs)
+    if(!is.null(deletes)) xy <- xy[-deletes, ]
+
+    lst$x <- xy$x
+    lst$y <- xy$y
+  }
+  return(lst)
+}
+
+
+# remap_xy_NA() -------------------------------------------------------
+
+
+remap_xy_NA <- function(lst){
+  if(lst$is.x && lst$is.y){
+    xy.lengths <- c(x = length(lst[["x"]]), y = length(lst[["y"]]))
+    xy.max <- which.max(xy.lengths) %>% names()
+    xy.min <- which.min(xy.lengths) %>% names()
+    xy.max.length <- length(lst[[xy.max]])
+
+    lst[[xy.min]] <- lst[[xy.min]][1L:xy.max.length]
+  }
+  return(lst)
+}
+
+
+# remap_xy_FALSE() ------------------------------------------------------
+
+
+remap_xy_FALSE <- function(lst){
+  if(lst$is.x && lst$is.y){
+    xy.lengths <- c(x = length(lst[[2]]), y = length(lst[[4]]))
+    xy.max <- which.max(xy.lengths) %>% names()
+    xy.min <- which.min(xy.lengths) %>% names()
+
+    quotient <- length(lst[[xy.max]]) %/% length(lst[[xy.min]])
+    remainder <- length(lst[[xy.max]]) %% length(lst[[xy.min]])
+    if.zero <- !is.na(remainder/remainder) %>% sum() # 0 if 0/0; 1 if !0/0
+
+    xy.quotient <- rep(lst[[xy.min]], quotient)
+    xy.remainder <- rep(lst[[xy.min]][1L:remainder], if.zero) # rep 0 or 1
+
+    lst[[xy.min]] <- c(xy.quotient, xy.remainder)
+  }
+  return(lst)
+}
+
+
+# remap_dots_TRUE() ---------------------------------------------------------
+
+
+remap_dots_TRUE <- function(lst){
+  if(lst$is.dots){
+    dots <- expand.grid(lst[-(1:5)], stringsAsFactors = F)
+    lst[6:length(lst)] <- dots[1:length(dots)]
+  }
+  return(lst)
+}
+
+
+# remap_dots_FALSE() --------------------------------------------------------
+
+
+remap_dots_FALSE <- function(lst){
+  if(lst$is.dots){
+    dots <- lst[-(1:5)]
+    no.recycle <- sapply(dots, length) %>%
+      which.max() %>%
+      lapply(dots[-.], function(x, y) x[1L:length(dots[[y]])], y = .)
+    lst[names(no.recycle)] <- no.recycle
+  }
+  return(lst)
+}
+
+
+# exclusive_dots() --------------------------------------------------------
+
+exclusive_dots <- function(lst){
+
+}
