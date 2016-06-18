9a3d66a8450c32e94d2bc96f4f57712f820b574b
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-18 16:18:32 -0500

    Removed anomalous text from git commit

--- R/pipes.R
+++ R/pipes.R
@@ -6,7 +6,11 @@
 
 
 `%>%` <- magrittr::`%>%`
-<<<<<<< HEAD
-=======
 
->>>>>>> d9d65edc2300ab09ec390b79f03a93672236c517
+
+
+# magrittr::`%<>%` --------------------------------------------------------
+
+# This is an alias for convenient calling of magrittr::`%<>%`
+
+`%<>%` <- magrittr::`%<>%`
