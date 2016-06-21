8f7fac26e68b3b20808187c9087c5019d4e941f7
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-21 13:04:02 -0500

    * remove old code * assign function name to to.name vector and aes-namming for-loop

--- R/aes.map.R
+++ R/aes.map.R
@@ -1,22 +1,7 @@
 
-# other.extractor() -------------------------------------------------------
-
-
-# lst.length <- lapply(seq_along(lst), function(x){
-#   length(x)
-# })
-
-# lapply(seq_along(lst), function(x){
-#   sapply(lst[[x]], `[[`, lst.length[x])
-# })
-
-# mapply(`[[`, lst[[1]], 1:lst.length)
-
-
 # extractor() -------------------------------------------------------------
 
 
-# this is a force; for loop should be replaced with lapply()
 extractor <- function(lst, num){
   rslt <- list()
   for(i in seq_len(num)){
@@ -29,8 +14,7 @@ extractor <- function(lst, num){
 # set_aes() ---------------------------------------------------------------
 
 
-set_aes <- function(...)
-{
+set_aes <- function(...){
   lst <- list(...)
   lapply(lst, function(x){
     x <- parse(text = x)
@@ -42,22 +26,16 @@ set_aes <- function(...)
 # name_aes() ------------------------------------------------------------
 
 
-to.name <- lapply(lst, function()x{
-  gsub("[0-9]+$", "", names(x))
-})
+names_aes <- function(lst){
+  to.name <- lapply(lst, function(x){
+    gsub("[0-9]+$", "", names(x))
+  })
 
-for(z in seq_along(aes.list)){
-  for(x in seq_along(aes.list[[z]])){
-    # rename <-  gsub("[0-9]+$", "", names(aes.list[[z]][x]))
-    for(y in seq_along(aes.list[[z]][[x]])){
-      names(aes.list[[z]][[x]])[y] <- to.name[[z]][y]
+  for(z in seq_along(lst)){
+    for(x in seq_along(lst[[z]])){
+      for(y in seq_along(lst[[z]][[x]])){
+        names(lst[[z]][[x]])[y] <- to.name[[z]][y]
+      }
     }
   }
 }
-
-
-# aes_map() ---------------------------------------------------------------
-
-
-# aes_structure() ---------------------------------------------------------
-
