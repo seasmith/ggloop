0cd967cbfc98b469d65422573a90437d332637e7
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-06-20 23:23:39 -0500

    Stage area for `%<L>%` compound L-plus operator; I have not written this one just yet

--- R/list.operator.R
+++ R/list.operator.R
@@ -14,9 +14,20 @@
 `%L+%` <- function(lhs, rhs){
   logic.1 <- is.list(lhs)
   logic.2 <- all(vapply(lhs, is.ggplot, FUN.VALUE = logical(1)))
+  logic.3 <- is.ggproto(rhs)
 
-  if(logic.1 & logic.2)
+  if(logic.1 && logic.2 && logic.3)
     lapply(seq_along(lhs), function(x, y){
       lhs[[x]] + y
     }, y = rhs)
 }
+
+
+
+# %<L>%() -----------------------------------------------------------------
+
+# The compound L-plus operator is  is a mix between magrittr::`%<>%`
+# and ggloop::`%L%`.
+# This operator has the same concept as L-plus, but reassigns the value of the
+# operation back into the original object, much like the compound operator in magrittr.
+
