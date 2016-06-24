d3e67a71000c4759fc9a3760567af1cd3c8fb49c
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-06-23 21:43:30 -0500

    Rework L-plus to deal with list of lists; need to include handling of lists and just ggplot objects

--- R/list.operator.R
+++ R/list.operator.R
@@ -11,17 +11,43 @@
 # on seq_along() the left-hand argument (the list of ggplot objects)
 
 
+# `%L+%` <- function(lhs, rhs){
+#   logic.1 <- is.list(lhs)
+#   logic.2 <- all(vapply(lhs, ggplot2::is.ggplot, FUN.VALUE = logical(1)))
+#   logic.3 <- ggplot2::is.ggproto(rhs)
+#
+#   if(logic.1 && logic.2 && logic.3)
+#     lapply(seq_along(lhs), function(x, y){
+#       lhs[[x]] + y
+#     }, y = rhs)
+# }
+
+# This needs some modification because the lhs will be
+# a list of lists of lists. This will require a second lapply() for
+# evaluation and adding the rhs into the lhs.
+
 `%L+%` <- function(lhs, rhs){
   logic.1 <- is.list(lhs)
-  logic.2 <- all(vapply(lhs, ggplot2::is.ggplot, FUN.VALUE = logical(1)))
+    if(!logic.1) stop("The list is not in proper format: must be list!")
+  logic.1.2 <- all(vapply(lhs, is.list, FUN.VALUE = logical(1)))
+    if(!logic.1.2) stop("The list is not in proper format: must be list of lists")
+  logic.2 <- all(sapply(lhs, function(x){
+    vapply(x, ggplot2::is.ggplot, FUN.VALUE = logical(1))
+    })
+    )
+    if(!logic.2) stop("The list of lists is not in proper format:
+                      each object in final list element must be of
+                      class ggplot (i.e. is.ggplot())")
   logic.3 <- ggplot2::is.ggproto(rhs)
 
-  if(logic.1 && logic.2 && logic.3)
-    lapply(seq_along(lhs), function(x, y){
-      lhs[[x]] + y
-    }, y = rhs)
-}
+    if(!logic.3) stop("rhs must be of class ggproto!")
 
+  lapply(seq_along(lhs), function(x, z){
+    lapply(lhs[[x]], function(y){
+      y + z
+    })
+  }, z = rhs)
+}
 
 
 # %<L>%() -----------------------------------------------------------------
