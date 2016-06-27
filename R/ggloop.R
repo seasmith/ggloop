20c2fad6f744a1118c27a0cd0b4e90d01011a491
Luke Smith <lukedansmi@sbcglobal.net>
2016-06-26 20:37:11 -0500

    Replace nested if's with logic and function vectors

--- R/ggloop.R
+++ R/ggloop.R
@@ -1,4 +1,10 @@
 
+# isFALSE() ---------------------------------------------------------------
+#
+# This is an abbreviation of identical(FALSE, x) to go along with isTRUE()
+
+isFALSE <- function(x) identical(FALSE, x)
+
 
 # ggloop() ----------------------------------------------------------------
 
@@ -12,22 +18,19 @@ ggloop <- function(data,
 }
 
 
-
 # aes_loop() --------------------------------------------------------------
 
 
 aes_loop <- function(data, x, y, remap_xy, remap_dots, ...){
   # place mapping argument values in a list
-  mappings <- aes_inputs2(data, x, y, ...)
+  mappings <- aes_assign(data, x, y, ...)
 
 
   # remap TRUE/FALSE/NA xy value pairs
-  if(is.na(remap_xy))
-    remap_xy_NA(mappings) else{
-      if(remap_xy)
-        remap_xy_TRUE(mappings) else
-          if(!remap_xy)
-            remap_xy_FALSE(mappings)
+  if(is.na(remap_xy)) remap_xy_NA(mappings) else{
+      if(remap_xy) remap_xy_TRUE(mappings) else{
+          if(!remap_xy) remap_xy_FALSE(mappings)
+      }
     }
 
   # remap TRUE/FALSE dots
@@ -43,7 +46,31 @@ aes_loop <- function(data, x, y, remap_xy, remap_dots, ...){
 }
 
 
+# aes_loop2() -------------------------------------------------------------
 
 
+aes_loop2 <- function(data, x, y, remap_xy, remap_dots, ...){
+  # place mapping argument values in a list
+  mappings <- aes_inputs2(data, x, y, ...)
+
+    # Logic vector indicating value of remap_xy argument
+    logic_xy <- c(isTRUE(remap_xy),
+                  isFALSE(remap_xy),
+                  is.na(remap_xy))
+    # Logic vector of the three possible functions to run
+    FUN_xy <- list(if_TRUE  = remap_xy_TRUE,
+                   if_FALSE = remap_xy_FALSE,
+                   if_NA    = remap_xy_NA)
+  # Subset FUN_xy to run the appropriate function
+  mappings <- FUN_xy[[which(logic_xy)]](mappings)
 
 
+    # Logic vector indicating value of remap_xy argument
+    logic_dots <- c(isTRUE(remap_dots),
+                    isFALSE(remap_dots))
+    # Logic vector of the two possible functions to run
+    FUN_dots <- list(if_TRUE = remap_dots_TRUE,
+                     if_FALSE = remap_dots_FALSE)
+  # Subset FUN_dots to run the appropriate function
+  mappings <- FUN_dots[[which(logic_dots)]](mappings)
+}
