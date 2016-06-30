7c6e4d426deab1dc8185067f1f48d5858f0cf0d8
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-06-29 21:33:31 -0500

    Added roxygen comments

--- R/list.operator.R
+++ R/list.operator.R
@@ -1,30 +1,16 @@
-
-# %L+%() ------------------------------------------------------------------
-
-
-# The %L+% (L-plus) operator, is a + operator wrapped by an lapply() loop.
-# The left hand argument is evaluated for two truths:
-#   is.list
-#   is.ggplot
-# If both these tests are TRUE then the right-hand argument is passed as
-# an additional argument in the lapply() loop. The lapply() loop is iterated
-# on seq_along() the left-hand argument (the list of ggplot objects)
-
-
-# `%L+%` <- function(lhs, rhs){
-#   logic.1 <- is.list(lhs)
-#   logic.2 <- all(vapply(lhs, ggplot2::is.ggplot, FUN.VALUE = logical(1)))
-#   logic.3 <- ggplot2::is.ggproto(rhs)
-#
-#   if(logic.1 && logic.2 && logic.3)
-#     lapply(seq_along(lhs), function(x, y){
-#       lhs[[x]] + y
-#     }, y = rhs)
-# }
-
-# This needs some modification because the lhs will be
-# a list of lists of lists. This will require a second lapply() for
-# evaluation and adding the rhs into the lhs.
+#' Add layers to \code{ggloop} outputs
+#'
+#' The \code{\%L+\%} (L-plus) operator, is a \code{+} operator wrapped by an \code{lapply()} loop.
+#' This function can also be a substitue for adding to "raw" \code{ggplot} objects.
+#'
+#' \code{\%L+\%} is typically used to add geoms, stats, and other ggplot2 features to
+#' the output of \code{ggloop()} (which is often a nested list of \code{ggplot} objects. However
+#' it is possible to use \code{\%L+\%} in place of where \code{+} would normally be used. This is
+#' due to the conditional statements present in \code{\%L+\%}'s structure.
+#'
+#' @param lhs Typically a \code{ggloop()} output (a nested list) but can also be a list of \code{ggplot}
+#' objects or a single \code{gglopt} object.
+#' @param rhs A geom, stat, or other layer feature from the \code{ggplot2} package.
 
 `%L+%` <- function(lhs, rhs){
   if(!ggplot2::is.ggproto(rhs)) stop("The rhs must be of class ggproto")
@@ -70,10 +56,8 @@
 }
 
 
-# %<L>%() -----------------------------------------------------------------
-
-# The compound L-plus operator is  is a mix between magrittr::`%<>%`
-# and ggloop::`%L%`.
-# This operator has the same concept as L-plus, but reassigns the value of the
-# operation back into the original object, much like the compound operator in magrittr.
+#' The \code{\%<L>\%} (compound L-plus) operator is  is a mix between magrittr::`%<>%`
+#' and ggloop::`%L%`.
+#' This operator has the same concept as L-plus, but reassigns the value of the
+#' operation back into the original object, much like the compound operator in magrittr.
 
