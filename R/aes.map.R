bea6fe4ddd8c781c485f8076f63687d0e5919885
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-06-30 16:37:01 -0500

    New function name_groups() to name the list elements in the ggloop() output list

--- R/aes.map.R
+++ R/aes.map.R
@@ -20,3 +20,22 @@ map_aes <- function(lst){
     })
     structure(mapping, class = "uneval")
 }
+
+
+
+# name_groups() -----------------------------------------------------------
+
+
+name_groups <- function(lst.1, lst.2, lst.3, dots.vector){
+  # lst.1 = nlst
+  # lst.2 = aes.inputs
+  # lst.3 = nputs.staged
+  # dots.vector = dots.vector
+  lapply(seq_along(lst.1), function(x){
+    names(lst.2)[x] <- lst.1[[x]] %>% unique()
+  })
+
+  .ncol <- as.numeric(summary(lst.3[dots.vector])[1])
+  lst.names <-  matrix(unlist(lst.3[dots.vector]),
+                        ncol = as.numeric(summary(lst.3[dots.vector])[1]))
+}
