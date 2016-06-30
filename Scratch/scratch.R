ddaf8f73a0dcfa66dd271b3c9572e395e89a928e
SeaSmith1018 <lukedansmi@sbcglobal.net>
2016-06-29 21:33:10 -0500

    Moved scratch.R to its own folder

---
+++ Scratch/scratch.R
@@ -0,0 +1,54 @@
+# extract first element
+sapply(nlst.lst[[1]], `[[`, 1)
+
+# do.call() attempt
+extractor <- function(lst, num){
+  sapply(lst, `[[`, num)
+}
+
+extractor2 <- function(lst, num){
+  lapply(lst, function(x){
+    sapply(x, function(y){
+
+    })
+  })
+}
+
+extractor3 <- function(lst, num){
+  rslt <- list()
+  for(i in seq_len(num)){
+  rslt[[i]] <- sapply(lst, `[[`, i)
+  }
+  return(rslt)
+}
+
+do.call(extractor,
+        list(lst = nlst.lst[[1]], 1:5))
+
+
+# Former set_aes() functions ----------------------------------------------
+
+# mcall2 <- function(...)
+# {
+#   structure(as.list(match.call()[-1]), class = "uneval")
+# }
+
+# mcall3 <- function(...)
+# {
+#   lst <- list(...)
+#   structure(lst, class = "uneval")
+# }
+
+# Other -------------------------------------------------------------------
+
+
+# if(lst$is.dots.chained){
+#   start <- which(names(lst) %in% "is.dots") + 1 # start of first dots argument
+#   end <- length(lst) - 1  # subtract for $is.dots.chained
+#
+#   lapply(seq_along(start:end), function(){
+#     Map(f = )
+#   })
+# } else{
+#
+# }
