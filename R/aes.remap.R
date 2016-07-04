6621b0747bad49a8013ea6a8e8811c86f9ae5fe4
Luke Smith <lukedansmi@sbcglobal.net>
2016-07-03 22:57:41 -0500

    Remove $is.x and $is.y; add list.pos(); minor spelling and grammer; add documentation

--- R/aes.remap.R
+++ R/aes.remap.R
@@ -1,7 +1,16 @@
 # remap_xy_TRUE() -----------------------------------------------------------
+#' Uses \code{expand.grid()} to create all possible combinations of xy pairings.
+#'
+#' Matching duplicates (xy pairings that contain identical xy values) will be
+#' tossed, and unordered duplicate pairs (xy pairings which match another xy
+#' pair (i.e. (mpg, cyl) == (cyl, mpg))) will be tossed.
+#'
+#' @param lst A list. The list passe will be the raw list generated from calling
+#' \code{aes_assign()} and is ran before a remap function for the any "dots"
+#' in the list.
 
 remap_xy_TRUE <- function(lst){
-  if(lst$is.x && lst$is.y){
+  if(exists("x", lst) && exists("y", lst)){
     xy <- expand.grid(y = lst$y, x = lst$x, stringsAsFactors = F)
 
     is.dupes <- mapply(FUN = c, xy$x, xy$y, SIMPLIFY = F) %>%
@@ -10,7 +19,6 @@ remap_xy_TRUE <- function(lst){
     dupes <- if(sum(is.dupes)) which(is.dupes) else NULL
 
     is.dubs <- xy$x == xy$y
-        # highly experimental
         is.dubs[is.na(is.dubs)] <- FALSE
     dubs <- if(sum(is.dubs)) which(is.dubs) else NULL
 
@@ -25,11 +33,20 @@ remap_xy_TRUE <- function(lst){
 
 
 # remap_xy_NA() -------------------------------------------------------------
-
+#' Attaches NA during recycling of the smaller of the two vectors.
+#'
+#' The smallest of the two vectors (\code{x} or \code{y}) will be recycled with
+#' NA instead of using the vector itself (similar to R's internal recycling
+#' mechanism).
+#'
+#' @param lst A list. The list passe will be the raw list generated from calling
+#' \code{aes_assign()} and is ran before a remap function for the any "dots"
+#' in the list.
 
 remap_xy_NA <- function(lst){
-  if(lst$is.x && lst$is.y){
+  if(exists("x", lst) && exists("y", lst)){
     # SHOULD I ADD SOMETHING TO DEAL WITH DUPES???
+      # what did i mean by that?
     xy.lengths <- c(x = length(lst$x), y = length(lst$y))
     xy.max <- which.max(xy.lengths) %>% names()
     xy.min <- which.min(xy.lengths) %>% names()
@@ -42,20 +59,28 @@ remap_xy_NA <- function(lst){
 
 
 # remap_xy_FALSE() ----------------------------------------------------------
-
+#' Mimicks R's internal recycling mechanism for the shorter of the two vectors.
+#'
+#' The smallest of the two vectors (\code{x} or \code{y}) will be recycled in
+#' a manner similar to R's internal recycling mechanism.
+#'
+#' @param lst A list. The list passe will be the raw list generated from calling
+#' \code{aes_assign()} and is ran before a remap function for the any "dots"
+#' in the list.
 
 remap_xy_FALSE <- function(lst){
-  if(lst$is.x && lst$is.y){
+  if(exists("x", lst) && exists("y", lst)){
     xy.lengths <- c(x = length(lst$x), y = length(lst$y))
     xy.max <- which.max(xy.lengths) %>% names()
     xy.min <- which.min(xy.lengths) %>% names()
+      if(xy.max == xy.min) return(lst)
 
     quotient <- length(lst[[xy.max]]) %/% length(lst[[xy.min]])
     remainder <- length(lst[[xy.max]]) %% length(lst[[xy.min]])
-    if.zero <- !is.na(remainder/remainder) %>% sum() # 0 if 0/0; 1 if !0/0
+    if.zero <- !is.na(remainder/remainder) %>% sum()
 
     xy.quotient <- rep(lst[[xy.min]], quotient)
-    xy.remainder <- rep(lst[[xy.min]][1L:remainder], if.zero) # rep 0 or 1
+    xy.remainder <- rep(lst[[xy.min]][1L:remainder], if.zero)
 
     lst[[xy.min]] <- c(xy.quotient, xy.remainder)
   }
@@ -68,12 +93,11 @@ remap_xy_FALSE <- function(lst){
 
 remap_dots_TRUE <- function(lst){
   if(lst$is.dots){
-    start <- which(names(lst) %in% "is.dots") + 1 # first dots argument
+    start <- list.pos("is.dots", lst) + 1
     end <- length(lst)
-    dots <- expand.grid(lst[end:start], stringsAsFactors = F)
-    lst[end:start] <- dots[1:length(dots)]
+    dots <- expand.grid(lst[end:start], stringsAsFactors = FALSE)
+    lst[end:start] <- dots[1L:length(dots)]
   }
-  lst$is.dots.chained <- TRUE
   return(lst)
 }
 
@@ -83,14 +107,13 @@ remap_dots_TRUE <- function(lst){
 
 remap_dots_FALSE <- function(lst){
   if(lst$is.dots){
-    start <- which(names(lst) %in% "is.dots") + 1 # first dots argument
+    start <- list.pos("is.dots", lst) + 1
     end <- length(lst)
     dots <- lst[start:end]
-    no.recycle <- sapply(dots, length) %>% which.max() %>%
+    recycled <- sapply(dots, length) %>% which.max() %>%
       lapply(dots[-.], function(x, y) x[1L:length(dots[[y]])], y = .)
-    lst[names(no.recycle)] <- no.recycle
+    lst[names(recycled)] <- recycled
   }
-  lst$is.dots.chained <- TRUE
   return(lst)
 }
 
@@ -99,7 +122,7 @@ remap_dots_FALSE <- function(lst){
 
 
 remap_dots_NA <- function(lst){
-  lst$is.dots.chained <- FALSE
+  # lst$is.dots.chained <- FALSE
   return(lst)
 }
 
