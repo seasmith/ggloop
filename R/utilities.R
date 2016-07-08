256f377010db8bd7c3c8d86dac4da3f2cedc62e9
Luke Smith <lukedansmi@sbcglobal.net>
2016-07-08 11:50:40 -0500

    New expand.grid2; create time.test() for testing evaluation time

--- R/utilities.R
+++ R/utilities.R
@@ -47,3 +47,86 @@ extract <- function(lst, num){
   }
   return(extracted)
 }
+
+
+
+# expand.grid2() ----------------------------------------------------------
+#
+#' A new version of an old favorite with some extra options
+#'
+#' \code{expand.grid2()} creates a combination data frame from vectors or lists
+#' but differs from the original \code{expand.grid()} in that it has two options
+#' for removing two different type of duplicates. \code{stringsAsFactors} is
+#' set to \code{TRUE}.
+#'
+#' @param rm.dupes Removes duplicated "rows". If \code{TRUE} (default) then
+#' rows that are unordered duplicates of other rows will be removed. i.e.
+#' \code{c("A", "B", "C")} is the same as \code{c("C", "B", "A")}
+#'  and any other combination of \code{"A"}, \code{"B"}, and \code{"C"}.
+#' @param rm.dubs Removes a row in which all elements are the same. If
+#' \code{TRUE} (default) then a row such as \code{c("A", "A", "A")} will be
+#' removed.
+
+expand.grid2 <- function(..., rm.dupes = TRUE, rm.dubs = TRUE){
+  args <- list(...)
+  nargs <- length(args)
+
+  grid <- expand.grid(args, stringsAsFactors = FALSE)
+    grid.names <- names(grid)
+    names(grid) <- NULL
+
+  grid.list <- extract(grid, nrow(grid))
+
+  # if(rm.dupes){
+  #   is.dupes <- lapply(seq_along(grid.list), function(x){
+  #     names(grid.list[[x]]) <- NULL
+  #     grid.list[[x]]}) %>%
+  #     lapply(sort) %>%
+  #     duplicated()
+  #   dupes <- if(sum(is.dupes)) which(is.dupes) else NULL
+  # } else{
+  #   dupes <- NULL
+  # }
+
+  if(rm.dupes){
+    is.dupes <- lapply(grid.list, sort) %>%
+      duplicated()
+    dupes <- if(sum(is.dupes)) which(is.dupes) else NULL
+  } else{
+    dupes <- NULL
+  }
+
+  if(rm.dubs){
+    is.dubs <- lapply(grid.list, duplicated) %>%
+      lapply(sum) %>%
+      `==`(nargs - 1)
+    dubs <- if(sum(is.dubs)) which(is.dubs) else NULL
+  } else{
+    dubs <- NULL
+  }
+
+  deletes <- c(dupes, dubs)
+  if(!is.null(deletes)) grid <- grid[-deletes, ]
+  names(grid) <- grid.names
+
+  return(grid)
+}
+
+
+
+# time.test() -------------------------------------------------------------
+#
+#' A simple test for evaluation speed.
+#'
+#' Tests the time it takes to evaluate an expression (such as a function). Uses
+#' \code{proc.time()} to keep track of all three different time types:
+#' \code{user}, \code{system}, and \code{elapsed}.
+#'
+#' @param x An expression, such as a function, arithmetic equation, etc.
+
+time.test <- function(x){
+  p1 <- proc.time()
+  eval(x)
+  p2 <- proc.time() - p1
+  print(p2)
+}
