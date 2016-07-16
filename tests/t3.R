fd90e58c16cf951c0c6de5c68a3785c12b99cfcb
Luke Smith <lukedansmi@sbcglobal.net>
2016-07-16 14:24:57 -0500

    Change functions for tests

--- tests/t3.R
+++ tests/t3.R
@@ -1,55 +1,55 @@
 
 # WORKS -------------------------------------------------------------------
-gg1.1 <- ggloop(mtcars,
-              aes_loop3(mpg:hp, mpg:hp, color = am:carb),
+gg1.1 <- ggloop2(mtcars,
+              aes_loop2(mpg:hp, mpg:cyl, color = am:carb),
               TRUE,
               FALSE)
 
 
 # WORKS -------------------------------------------------------------------
-gg1.2 <- ggloop(mtcars,
-              aes_loop3(mpg:hp, mpg:hp, color = am:carb),
+gg1.2 <- ggloop2(mtcars,
+              aes_loop2(mpg:disp, mpg:hp, color = am:carb),
               FALSE,
               FALSE)
 
 
 # WORKS -------------------------------------------------------------------
-gg2.1 <- ggloop(mtcars,
-              aes_loop3(mpg:hp, mpg:hp, color = am:carb, size = vs:am),
+gg2.1 <- ggloop2(mtcars,
+              aes_loop2(mpg:hp, mpg:hp, color = am:carb, size = vs:am),
               TRUE,
               FALSE)
 
 
 # WORKS -------------------------------------------------------------------
-gg2.2 <- ggloop(mtcars,
-              aes_loop3(mpg:hp, mpg:hp, color = am:carb, size = vs:am),
+gg2.2 <- ggloop2(mtcars,
+              aes_loop2(mpg:hp, mpg:hp, color = am:carb, size = vs:am),
               TRUE,
               TRUE)
 
 
 # FAILS -------------------------------------------------------------------
-gg3.1 <- ggloop(mtcars,
-                aes_loop3(mpg:hp, mpg:hp),
+gg3.1 <- ggloop2(mtcars,
+                aes_loop2(mpg, cyl:hp),
                 TRUE,
                 FALSE)
 
 
 # FAILS -------------------------------------------------------------------
-gg3.2 <- ggloop(mtcars,
-                aes_loop3(mpg:hp, mpg:hp),
+gg3.2 <- ggloop2(mtcars,
+                aes_loop2(mpg:hp, c(1,3,5,7)),
                 TRUE,
                 TRUE)
 
 
 # FAILS -------------------------------------------------------------------
-gg4.1 <- ggloop(mtcars,
-                aes_loop3(mpg:hp, mpg:hp),
+gg4.1 <- ggloop2(mtcars,
+                aes_loop2(mpg:hp, mpg:hp),
                 FALSE,
                 FALSE)
 
 
 # FAILS -------------------------------------------------------------------
-gg4.2 <- ggloop(mtcars,
-                aes_loop3(mpg:hp, mpg:hp),
+gg4.2 <- ggloop2(mtcars,
+                aes_loop2(mpg:hp, mpg:hp),
                 FALSE,
                 TRUE)
