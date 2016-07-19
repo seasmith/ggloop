4097e0094841813065024b9d418a819bd0c0cce1
Luke Smith <lukedansmi@sbcglobal.net>
2016-07-19 14:10:08 -0500

    more work on understanding function environments

--- R/scratch2.R
+++ R/scratch2.R
@@ -1,13 +1,38 @@
-f1 <- function(x, y){
-  if(hasArg(x)) x <- substitute(x)
-  if(hasArg(y)) y <- substitute(y)
+outer.fun <- function(arg1, arg2){
+  eval(arg2)
+  print(arg1)
 
-  function(){
-    ran.fun <- f2(x, y)
-  }
+
 }
 
-f2 <- function(x, y){
-  if(missing("x")) print(x)
-  if(missing("y")) print(z)
+inner.fun <- function(iarg){
+  return(iarg)
 }
+
+# # Why does the following input exist?
+# # Because it is a promise?
+# # Both eval(x) and get0(x) throw an error.
+# # eval(x) would throw an error if non-standard evaluation
+# # were used (similar to dplyr's mpg:hp column naming syntax).
+# f3 <- function(x){
+#   f4(x)
+# }
+#
+# f4 <- function(x){
+#
+#   exists <- exists("x")
+#   not.missing <- !missing("x")
+#   is.promise <- pryr::is_promise(x)
+#   try <- tryCatch({
+#     get0("x")
+#     TRUE
+#   }, error = function(e){
+#     FALSE
+#   })
+#
+#   print(exists)
+#   print(not.missing)
+#   print(is.promise)
+#   print(try)
+# }
+#
