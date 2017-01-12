026d82a23b7ea0c41453d8f59cea55541a65c19e
Luke Smith <lukedansmi@sbcglobal.net>
2017-01-12 03:22:14 -0600

    Place dots inside Map(); rename get_xy to get_aes

--- R/aes.wrangle.R
+++ R/aes.wrangle.R
@@ -50,7 +50,7 @@ aes_eval <- function(x, y, dots, vars) {
   names_list <- stats::setNames(as.list(seq_along(vars)), vars)
 
   # Function - retrieve the unevaluated x and y variables
-  get_xy <- function(aes, aes.exists, names) {
+  get_aes <- function(aes, aes.exists, names) {
 
     if (aes.exists) {
 
@@ -77,72 +77,43 @@ aes_eval <- function(x, y, dots, vars) {
       }
 
       return(unlist(aes.eval, use.names = FALSE))
+
     } else {
+
       return(NULL)
+
     }
   }
 
-  # Get names .
-  xy.eval <- Map(get_xy, list(x, y), xy.exists, list(vars))
+  # Get xy names.
+  xy.eval <- Map(get_aes, list(x, y), xy.exists, list(vars))
   names(xy.eval) <- c("x", "y")
 
-  ### Capture dots if exist
+  ### Get dot names (if exist).
   if (length(dots)) {
-    # - Capture names (names will be lost by the dots.dplyr.eval lapply()).
-    # - Strip c().
-    # - Creat list to hold evaluations.
-    dots.names <- names(dots)
-    dots       <- lapply(dots, function(x) if (is_c(x)) x[-1L] else list(x))
-    dots.eval  <- list()
 
-    # Determine variables by expression type: ggplot2 (gg2) or dplyr.
-    dots.gg2   <- lapply(dots, function(x) which_gg2(x) %R% FALSE)
-    find_dplyr <- function(x, y) {
-      if (isFALSE(y)) seq_along(x)
-      else seq_along(x)[-y] %R% FALSE
-    }
-    dots.dplyr <- Map(find_dplyr, x = dots, y = dots.gg2)
+    dots.eval <- list()
+    dots.eval <- Map(get_aes, dots, list(TRUE), list(vars))
+    is.dots   <- TRUE
 
-    # - "Evaluate" ggplot2-like expressions.
-      eval_gg2 <- function(x, y) {
-        d.eval <- list()
-        # removed rev(abs(y)) and rev(y)
-        d.eval[abs(y)] <- vapply(x[y], deparse, character(1)) %R% NULL
-      }
-    dots.gg2.eval   <- Map(eval_gg2, x = dots, y = dots.gg2)
-
-    # - "Evaluate" dplyr-like expressions
-    dots.dplyr.eval <- lapply(seq_along(dots.dplyr), function(i) {
-      if (isFALSE(dots.dplyr[[i]])) NULL
-      else {
-        d.eval <- list()
-        d.eval[dots.dplyr[[i]]] <- lapply(dots.dplyr[[i]], function(j) {
-          messy_eval(dots[[i]][[j]], vars, names_list)
-          })
-        d.eval
-      }
-    })
-
-    # Combine dots "Evaluations"
-    dots.eval <- lapply(seq_along(dots), function(x) {
-      c(unlist(dots.gg2.eval[[x]]), unlist(dots.dplyr.eval[[x]]))
-      })
-
-    names(dots.eval) <- dots.names
-    is.dots <- TRUE
   } else {
+
     dots.eval <- NULL
     is.dots   <- FALSE
+
   }
+
   # list values and logical existance of ... arguments
   mappings <- c(list(x = xy.eval[["x"]],
                      y = xy.eval[["y"]],
                      is.dots = is.dots),
                 dots.eval)
 
+  # Get rid of anything that isn't there (NULLs).
   mappings <- mappings[!vapply(mappings, is.null, logical(1))]
 
   return(mappings)
+
 }
 
 
@@ -165,12 +136,14 @@ aes_eval <- function(x, y, dots, vars) {
 #' nested components) correspond to the \code{x} and \code{y} vectors.
 
 aes_group <- function(lst) {
+
   parent <- parent.frame()
   env    <- new.env(parent = parent)
 
   env$xy <- lst[stats::na.omit(c(list.pos("x", lst), list.pos("y",lst)))]
 
   if (lst[["is.dots"]]) {
+
     start <- list.pos("is.dots", lst) + 1
     end   <- length(lst)
     env$dots.vector <- start:end
@@ -196,12 +169,18 @@ aes_group <- function(lst) {
       iterator <- unit.vector + x - 1
       c(env$xy, dots.list[iterator])
     })
+
     env$groups <- rename_inputs(env$groups)
+
   } else {
+
     env$groups      <- env$xy
     env$dots.vector <- NULL
     env$rep.num     <- NULL
+
   }
+
   # xy, dots.vector, rep.num, groups
   return(env)
+
 }
